404fb68234ea3e968469e78f49216d3925aed4b9
mspolermo <mspolermo@gmail.com>
2024-06-04 18:43:37 +0500

    logic for view and edit form for post added'

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <template>
-    <div>
-        <div>
-            <button @click="addLike">Like</button>
-            <button @click="addDislike">Dislike</button>
+    <div class="app">
+        <form @submit.prevent>
+            <h4>Создание поста</h4>
+            <input v-bind:value="title" @input="inputTitle" class="input" type="text" placeholder="Название"/>
+            <input v-bind:value="body" @input="body = $event.target.value" class="input" type="text" placeholder="Описание"/>
+            <button class="btn" @click="createPost">Создать</button>
+        </form>
+        <div class="post" v-for="post in posts">
+            <div>
+                <strong>Название: </strong>
+                <span>{{ post.title }}</span>
+            </div>
+            <div>
+                <strong>Описание: </strong>
+                <span>{{ post.body }}</span>
+            </div>
         </div>
-        <div>Кол-во лайков: <strong>{{ likes }}</strong></div>
-        <div>Кол-во дизлайков: <strong>{{ dislikes }}</strong></div>
     </div>
 </template>
 
@@ -13,20 +23,61 @@
 export default {
     data() {
         return {
-            likes: 0,
-            dislikes: 0
+            posts: [
+                {id: 1, title: 'JavaScript', body: 'Описание поста'},
+                {id: 2, title: 'JavaScript 2', body: 'Описание поста 2'},
+                {id: 3, title: 'JavaScript 3', body: 'Описание поста 3'}
+            ],
+            title: '',
+            body: ''
         }
     },
     methods: {
-        addLike() {
-            this.likes += 1;
+        createPost() {
+            const newPost = {
+                id: Date.now(),
+                title: this.title,
+                body: this.body
+            }
+            this.posts.push(newPost);
+            this.title = "";
+            this.body = "";
         },
-        addDislike() {
-            this.dislikes += 1;
-        }
+        inputTitle(e) {this.title = event.target.value},
     }
 }
 </script>
 
 <style>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+.app {
+    padding: 20px;
+}
+.post {
+    margin-top: 15px;
+    padding: 15px;
+    border: 2px solid teal;
+}
+form {
+    display: flex;
+    flex-direction: column;
+}
+.input {
+    margin-top: 15px;
+    width: 100%;
+    border: 1px solid teal;
+    padding: 10px 15px;
+}
+.btn {
+    margin-top: 15px;
+    align-self: flex-end;
+    padding: 10px 15px;
+    background: none;
+    color: teal;
+    border: 1px solid teal;
+}
 </style>
