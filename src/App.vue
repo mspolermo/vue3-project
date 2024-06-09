c29d9cbf26637280088f8e53f4e9450ab293fbe0
mspolermo <mspolermo@gmail.com>
2024-06-09 18:11:11 +0500

    dynamic pagination fix

--- src/App.vue
+++ src/App.vue
@@ -1,138 +1,120 @@
 <template>
-    <div class="app">
-      <h1>Страница с постами</h1>
-      <my-input v-model="searchQuery" placeholder="Поиск..."/>
-      <div class="app__btns">
-        <my-button @click="showDialog">Создать пост</my-button>
-        <my-select v-model="selectedSort" :options="sortOptions" />
-      </div>
-      <my-dialog v-model:show="dialogVisible">
-        <post-form @create="createPost"/>
-      </my-dialog>
-      <post-list :posts="sortedAndSearchedPosts" @remove="removePost" v-if="!isPostLoading"/>
-      <div v-else>Загрузка</div>
-      <div ref="observer" class="observer"></div>
-<!--      <div class="page__wrapper">-->
-<!--        <div-->
-<!--            v-for="pageNumber in totalPages"-->
-<!--            :key="pageNumber"-->
-<!--            class="page"-->
-<!--            :class="{-->
-<!--              'current-page': page === pageNumber-->
-<!--            }"-->
-<!--            @click="changePage(pageNumber)"-->
-<!--        >-->
-<!--          {{ pageNumber }}-->
-<!--        </div>-->
-<!--      </div>-->
+  <div class="app">
+    <h1>Страница с постами</h1>
+    <my-input v-model="searchQuery" placeholder="Поиск..."/>
+    <div class="app__btns">
+      <my-button @click="showDialog">Создать пост</my-button>
+      <my-select v-model="selectedSort" :options="sortOptions" />
     </div>
+    <my-dialog v-model:show="dialogVisible">
+      <post-form @create="createPost"/>
+    </my-dialog>
+    <post-list :posts="sortedAndSearchedPosts" @remove="removePost" v-if="!isPostLoading"/>
+    <div v-else>Загрузка</div>
+    <div ref="observer" class="observer"></div>
+  </div>
 </template>
 
 <script>
 import PostForm from "@/components/PostForm.vue";
 import PostList from "@/components/PostList.vue";
 import axios from "axios";
+
 export default {
   components: {
     PostForm, PostList
   },
-    data() {
-      return {
-        posts: [],
-        dialogVisible: false,
-        isPostLoading: false,
-        selectedSort: '',
-        searchQuery: '',
-        page: 1,
-        limit: 10,
-        totalPages: 0,
-        sortOptions: [
-          {value: 'title', name: 'По названию'},
-          {value: 'body', name: 'По содержимому'}
-        ]
-      }
-    },
-    methods: {
-      // changePage(pageNumber) {
-      //   this.page = pageNumber;
-      // },
-      createPost(post) {
-        this.posts.push(post);
-        this.dialogVisible = false;
-      },
-      removePost(post) {
-        this.posts = this.posts.filter(p => p.id !== post.id)
-      },
-      showDialog() {
-        this.dialogVisible = true;
-      },
-      async fetchPosts() {
-        this.isPostLoading = true;
-        try {
-            const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
-              params: {
-                _page: this.page,
-                _limit: this.limit
-              }
-            });
-            this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit);
-            this.posts = response.data;
-        } catch (e) {
-          alert('Ошибка получения постов')
-        }
-        finally {
-          this.isPostLoading = false;
-        }
-      },
-      async loadMorePosts() {
-        this.page += 1;
-        this.isPostLoading = true;
-        try {
-          const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
-            params: {
-              _page: this.page,
-              _limit: this.limit
-            }
-          });
-          this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit);
-          this.posts = [...this.posts , response.data];
-        } catch (e) {
-          alert('Ошибка получения постов')
-        }
-        finally {
-          this.isPostLoading = false;
-        }
-      },
-    },
-    mounted() {
-      this.fetchPosts();
-      const options = {
-        rootMargin: '0px',
-        threshold: 1.0
-      }
-      const callback = (entries, observer) => {
-        if (entries[0].isIntersecting) {
-          this.loadMorePosts()
-        }
-      }
-      const observer = new IntersectionObserver(callback, options);
-      observer.observe(this.$refs.observer)
-    },
-    computed: {
-      sortedPosts() {
-        return [...this.posts].sort((post1, post2) =>
-            post1[this.selectedSort]?.localeCompare(post2[this.selectedSort])
-        )
-      },
-      sortedAndSearchedPosts() {
-        return this.sortedPosts.filter(post => post.title.toLowerCase().includes(this.searchQuery.toLowerCase()))
-      }
-    },
-    watch: {
-      // page() {
-      //   this.fetchPosts()
-      // }
+  data() {
+    return {
+      posts: [],
+      dialogVisible: false,
+      isPostLoading: false,
+      selectedSort: '',
+      searchQuery: '',
+      page: 1,
+      limit: 10,
+      totalPages: 0,
+      sortOptions: [
+        {value: 'title', name: 'По названию'},
+        {value: 'body', name: 'По содержимому'}
+      ]
     }
+  },
+  methods: {
+    createPost(post) {
+      this.posts.push(post);
+      this.dialogVisible = false;
+    },
+    removePost(post) {
+      this.posts = this.posts.filter(p => p.id !== post.id)
+    },
+    showDialog() {
+      this.dialogVisible = true;
+    },
+    async fetchPosts() {
+      this.isPostLoading = true;
+      try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+          params: {
+            _page: this.page,
+            _limit: this.limit
+          }
+        });
+        this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit);
+        this.posts = response.data;
+      } catch (e) {
+        alert('Ошибка получения постов')
+      }
+      finally {
+        this.isPostLoading = false;
+      }
+    },
+    async loadMorePosts() {
+      this.page += 1;
+      this.isPostLoading = true;
+      try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+          params: {
+            _page: this.page,
+            _limit: this.limit
+          }
+        });
+        this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit);
+        this.posts = [...this.posts, ...response.data];
+      } catch (e) {
+        alert('Ошибка получения постов')
+      }
+      finally {
+        this.isPostLoading = false;
+      }
+    },
+  },
+  mounted() {
+    this.fetchPosts();
+    const options = {
+      rootMargin: '0px',
+      threshold: 1.0
+    }
+    const callback = (entries, observer) => {
+      if (entries[0].isIntersecting) {
+        this.loadMorePosts()
+      }
+    }
+    const observer = new IntersectionObserver(callback, options);
+    observer.observe(this.$refs.observer)
+  },
+  computed: {
+    sortedPosts() {
+      return [...this.posts].sort((post1, post2) =>
+          post1[this.selectedSort]?.localeCompare(post2[this.selectedSort])
+      );
+    },
+    sortedAndSearchedPosts() {
+      return this.sortedPosts.filter(post =>
+          post.title && post.title.toLowerCase().includes(this.searchQuery.toLowerCase())
+      );
+    }
+  }
 }
 </script>
 
