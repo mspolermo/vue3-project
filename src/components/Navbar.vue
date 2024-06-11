4629c645cc008248ab926509f634813a98704b92
mspolermo <mspolermo@gmail.com>
2024-06-11 18:39:06 +0500

    router added

---
+++ src/components/Navbar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="navbar">
+    <div @click="$router.push('/')" >На главную</div>
+    <div class="navbar__btns">
+      <my-button class="navbar__btn" @click="$router.push('/posts')">Посты</my-button>
+      <my-button class="navbar__btn" @click="$router.push('/about')">О сайте</my-button>
+    </div>
+  </div>
+</template>
+
+<script>
+import MyButton from "@/components/UI/MyButton.vue";
+
+export default {
+  components: {MyButton}
+
+}
+</script>
+
+<style scoped>
+.navbar {
+  height: 50px;
+  background: lightgray;
+  box-shadow: 2px 2px 4px gray;
+  display: flex;
+  align-items: center;
+  padding: 0 15px;
+}
+.navbar__btns {
+  margin-left: auto;
+}
+.navbar__btn {
+  margin-left: 20px;
+}
+</style>
---
+++ src/pages/PostPage.vue
@@ -0,0 +1,138 @@
+<template>
+  <div >
+    <h1>Страница с постами</h1>
+    <my-input v-model="searchQuery" placeholder="Поиск..."/>
+    <div class="app__btns">
+      <my-button @click="showDialog">Создать пост</my-button>
+      <my-select v-model="selectedSort" :options="sortOptions" />
+    </div>
+    <my-dialog v-model:show="dialogVisible">
+      <post-form @create="createPost"/>
+    </my-dialog>
+    <post-list :posts="sortedAndSearchedPosts" @remove="removePost" v-if="!isPostLoading"/>
+    <div v-else>Загрузка</div>
+    <div ref="observer" class="observer"></div>
+  </div>
+</template>
+
+<script>
+import PostForm from "@/components/PostForm.vue";
+import PostList from "@/components/PostList.vue";
+import axios from "axios";
+
+export default {
+  components: {
+    PostForm, PostList
+  },
+  data() {
+    return {
+      posts: [],
+      dialogVisible: false,
+      isPostLoading: false,
+      selectedSort: '',
+      searchQuery: '',
+      page: 1,
+      limit: 10,
+      totalPages: 0,
+      sortOptions: [
+        {value: 'title', name: 'По названию'},
+        {value: 'body', name: 'По содержимому'}
+      ]
+    }
+  },
+  methods: {
+    createPost(post) {
+      this.posts.push(post);
+      this.dialogVisible = false;
+    },
+    removePost(post) {
+      this.posts = this.posts.filter(p => p.id !== post.id)
+    },
+    showDialog() {
+      this.dialogVisible = true;
+    },
+    async fetchPosts() {
+      this.isPostLoading = true;
+      try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+          params: {
+            _page: this.page,
+            _limit: this.limit
+          }
+        });
+        this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit);
+        this.posts = response.data;
+      } catch (e) {
+        alert('Ошибка получения постов')
+      }
+      finally {
+        this.isPostLoading = false;
+      }
+    },
+    async loadMorePosts() {
+      this.page += 1;
+      try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+          params: {
+            _page: this.page,
+            _limit: this.limit
+          }
+        });
+        this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit);
+        this.posts = [...this.posts, ...response.data];
+      } catch (e) {
+        alert('Ошибка получения постов')
+      }
+    },
+  },
+  mounted() {
+    this.fetchPosts();
+    const options = {
+      rootMargin: '0px',
+      threshold: 1.0
+    }
+    const callback = (entries, observer) => {
+      if (entries[0].isIntersecting && this.page < this.totalPages) {
+        this.loadMorePosts()
+      }
+    }
+    const observer = new IntersectionObserver(callback, options);
+    observer.observe(this.$refs.observer)
+  },
+  computed: {
+    sortedPosts() {
+      return [...this.posts].sort((post1, post2) =>
+          post1[this.selectedSort]?.localeCompare(post2[this.selectedSort])
+      );
+    },
+    sortedAndSearchedPosts() {
+      return this.sortedPosts.filter(post =>
+          post.title && post.title.toLowerCase().includes(this.searchQuery.toLowerCase())
+      );
+    }
+  }
+}
+</script>
+
+<style>
+.app__btns {
+  margin: 15px 0;
+  display: flex;
+  justify-content: space-between;
+}
+.page__wrapper {
+  display: flex;
+  margin-top: 15px;
+}
+.page {
+  border: 1px solid black;
+  padding: 10px;
+}
+.current-page {
+  border: 2px solid teal;
+}
+.observer {
+  height: 30px;
+  background: green;
+}
+</style>
