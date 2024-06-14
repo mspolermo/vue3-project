c3cd2bfda3e0d273d9c8711b0e1515bcd7d1e5c4
mspolermo <mspolermo@gmail.com>
2024-06-14 17:50:51 +0500

    custom dirictives added

---
+++ src/dirictives/index.js
@@ -0,0 +1,7 @@
+import VFocus from "@/dirictives/VFocus";
+import VIntersection from "@/dirictives/VIntersection";
+
+export default [
+    VFocus,
+    VIntersection
+]
--- src/pages/PostPage.vue
+++ src/pages/PostPage.vue
@@ -11,7 +11,7 @@
     </my-dialog>
     <post-list :posts="sortedAndSearchedPosts" @remove="removePost" v-if="!isPostLoading"/>
     <div v-else>Загрузка</div>
-    <div ref="observer" class="observer"></div>
+    <div v-intersection="loadMorePosts" class="observer"></div>
   </div>
 </template>
 
@@ -87,17 +87,6 @@ export default {
   },
   mounted() {
     this.fetchPosts();
-    const options = {
-      rootMargin: '0px',
-      threshold: 1.0
-    }
-    const callback = (entries, observer) => {
-      if (entries[0].isIntersecting && this.page < this.totalPages) {
-        this.loadMorePosts()
-      }
-    }
-    const observer = new IntersectionObserver(callback, options);
-    observer.observe(this.$refs.observer)
   },
   computed: {
     sortedPosts() {
